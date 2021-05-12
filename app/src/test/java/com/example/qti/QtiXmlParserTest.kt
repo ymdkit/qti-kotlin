d2862a450c33055af6cf77b5c66afb3c7db03c4a
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2021-05-12 15:28:06 +0900

    Add test

--- app/src/test/java/com/example/qti/QtiXmlParserTest.kt
+++ app/src/test/java/com/example/qti/QtiXmlParserTest.kt
@@ -1,5 +1,6 @@
 package com.example.qti
 
+import com.example.qti.resource.*
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.io.File
@@ -10,9 +11,30 @@ class ExampleUnitTest {
     fun parseQti() {
 
         val parser = QtiXmlParser()
-        val file = File("src/test/resources/test_qti.txt")
-        val questions = parser.parse(FileInputStream(file))
+        val file = File("src/test/resources/test_qti.xml")
+        val actual = parser.parse(FileInputStream(file))
 
-        assertEquals(questions[0].title, "Question")
+        val expect =
+            QtiAssessmentItem(
+                response = QtiResponse(listOf("A")),
+                outcome = QtiOutcome(1f),
+                itemBody = QtiItemBody(
+                    prompt = QtiPrompt("<p>Of the following hormones, which is produced by the adrenal glands?</p>"),
+                    interaction = SimpleChoiceQtiInteraction(
+                        choices = listOf(
+                            "A" to "Epinephrine",
+                            "B" to "Glucagon",
+                            "C" to "Insulin",
+                            "D" to "Oxytocin",
+                        ),
+                        maxChoices = 1,
+                        minChoices = 1
+                    )
+                )
+            )
+
+        assertEquals(expect.response, actual.response)
+        assertEquals(expect.outcome, actual.outcome)
+        assertEquals(expect.itemBody, actual.itemBody)
     }
 }
